28d72bf9c32f26dfdbcf42a72efa84bd06158e8b
cparros <celparros@gmail.com>
2021-03-24 14:14:53 -0400

    updated db seeds to reflect workout

--- db/seed.js
+++ db/seed.js
@@ -13,7 +13,7 @@ module.exports = (db) => {
       password: process.env.USER_PWD,
       isAdmin: false,
     }).then(() => {
-      db.Example.create({
+      db.Workout.create({
         text: "Sample item",
         description: "Adam can't see this",
         UserId: 2,
