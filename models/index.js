db9b5d200bf7a4d897b2a8220efaebfa7504e50e
cparros <celparros@gmail.com>
2021-03-24 17:20:24 -0400

    var reference fix

--- models/index.js
+++ models/index.js
@@ -4,13 +4,17 @@ const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development' || 'production';
 const config = require(path.join(__dirname, '/../config/config.js'))[env];
 const db = {};
 let sequelize;
 
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+console.log("config: ", JSON.stringify(config))
+console.log("first" + config.useEnvVariable)
+console.log("second" + config)
+if (config.useEnvVariable) {
+  sequelize = new Sequelize(process.env[config.useEnvVariable], config);
+  console.log(config)
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
